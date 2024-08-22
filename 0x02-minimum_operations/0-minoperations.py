9e172eba4c8f72cd8c7d11d2dc6f9a32aa2e0d3c
Eskindir H <eskindirhaileliul@gmail.com>
2024-08-22 12:56:32 +0000

    done

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+"""
+This is a function to calculate the minimum opertion
+"""
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
